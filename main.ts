817b25102748949ec6935b35826db86b4c7bef0b
BuchiProf <46644031+BuchiProf@users.noreply.github.com>
2026-04-01 10:03:10 +0200

    premier commit

--- main.ts
+++ main.ts
@@ -1,1 +1,38 @@
+/**
+ * Extension pour le module 4-Relais I2C de Seeed Studio
+ */
+// % color="#03AA14" icon="\uf013" block="Relais Seeed"
+namespace seeed4Relay {
 
+    // Menu déroulant pour l'état
+    export enum RelayState {
+        // % block="allumé"
+        On = 1,
+        // % block="éteint"
+        Off = 0
+    }
+
+    // NOUVEAU : Menu déroulant pour le choix du relais
+    export enum RelayNumber {
+        // % block="sw1"
+        Relay1 = 1,
+        // % block="sw2"
+        Relay2 = 2,
+        // % block="sw3"
+        Relay3 = 3,
+        // % block="sw4"
+        Relay4 = 4
+    }
+
+    /**
+     * Change l'état d'un relais spécifique via menu déroulant
+     */
+    // % block="mettre le relais %num à %state"
+    // % num.defl=RelayNumber.Relay1
+    export function controlRelay(num: RelayNumber, state: RelayState): void {
+        let buf = pins.createBuffer(2);
+        buf[0] = num;   // Utilise la valeur de l'enum (1, 2, 3 ou 4)
+        buf[1] = state; // Utilise 1 ou 0
+        pins.i2cWriteBuffer(0x11, buf);
+    }
+}
